08c4658bd199750a9f31067bf99468132d2bce71
Kenny <keggex101@gmail.com>
2025-08-26 15:24:21 +0100

    Created a 16 x 16 grid with a hovering effect

---
+++ script.js
@@ -0,0 +1,14 @@
+// your JavaScript file
+const container = document.querySelector("#container");
+
+
+
+i = 0
+j = 16*16
+
+while (i < j){
+    const div = document.createElement("div");
+    container.appendChild(div);
+    i ++
+}
+
