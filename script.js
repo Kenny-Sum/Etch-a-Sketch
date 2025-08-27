4d6b00790a8bfab8680563ac7c37b7670fc4e9f0
Kenny <keggex101@gmail.com>
2025-08-27 10:54:17 +0100

    Added a input num btn and submit btn for grid. Edits made to css.

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-// your JavaScript file
+let numInput;
+
+document.getElementById("numSubmit").onclick = function(){
+    numInput = document.getElementById("numInput").value;
+    console.log(numInput);
+}
+
+
+
+// container
 const container = document.querySelector("#container");
 
-
-
 i = 0
-j = 16*16
+j = 2*2
 
 while (i < j){
     const div = document.createElement("div");
