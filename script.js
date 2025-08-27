2316adf85d73492ef70b1968e0b300ba203b7081
Kenny <keggex101@gmail.com>
2025-08-27 14:00:39 +0100

    Allows user to hover and change square colour permanently and adds resetgrid function.

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
-
+const container = document.querySelector("#container");
+const square = document.querySelector(".square");
 
 function createGrid(){
+    container.innerHTML =""; // Removes all of the elements within the element container.
+
     // Input number and submit
-    let sizeInput = document.getElementById("sizeInput").value;
-    console.log(sizeInput)
-    
+    let sizeInput = document.getElementById("sizeInput").value; // let sizeInput be equal to the value inputted
 
     // container
-    const container = document.querySelector("#container");
+    
 
     let grid = sizeInput * sizeInput
     console.log(grid)
-    const gridSize = 700;
-
-    const square = document.querySelector(".square");
+    const containerSize = 700;
 
     for (let i = 0; i < grid; i++){
-        const square = document.createElement("div");
-        square.classList.add("square")
-        square.style.width = `${gridSize/sizeInput}px`;
-        square.style.height = `${gridSize/sizeInput}px`;
-        container.appendChild(square);
+        const square = document.createElement("div"); // Creates a sqaure in the container
+        square.classList.add("square") // Adds a classname (sqaure) to the sqaure div
+        square.style.width = `${containerSize/sizeInput}px`; // sets the width of each sqaure
+        square.style.height = `${containerSize/sizeInput}px`; // sets the height of each sqaure
+        container.appendChild(square); // Appends the sqaure to the container 
+        
+        // Changes background to black when hovering.
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'black';
+        });
     }
 }
 
+function resetGrid(){
+    container.innerHTML = "";
+}
 
+// Reset Grid
+document.getElementById('reset').onclick = resetGrid;
+
+// Clicking button with ID numSubmit creates a grid
 document.getElementById('numSubmit').onclick = createGrid;
